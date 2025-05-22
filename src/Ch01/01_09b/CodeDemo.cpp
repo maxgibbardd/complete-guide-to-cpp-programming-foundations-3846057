cbe92798b422c6fa12d510daf4f520c00657610c
Maxwell Gibbard <134660926+maxgibbardd@users.noreply.github.com>
2025-05-22 21:21:45 +0000

    name input program implemented

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,18 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
     std::cout << "Hi There!" << std::endl;
+
+    // start of my code
+    std::string name;
+
+    std::cout << "Please enter your name: ";
+    std::cin >> name;
+    std::cout << "Your name is " << name << "!" << std::endl;
+    // end of my code
 
     std::cout << std::endl << std::endl;
     return 0;
