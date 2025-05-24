27c876d105f5b25f29bf0575417aa92550576ba9
Maxwell Gibbard <134660926+maxgibbardd@users.noreply.github.com>
2025-05-24 22:55:06 +0000

    ch2 vids 5, 8, 9

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,7 +4,27 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+
+    // displaying each variable on it's own line
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+
+    // now displaying different expressions on it's own line
+    my_flag = true;
+    std::cout << "my_flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -5,14 +5,20 @@
 #include <iostream>
 #include <cstdint>
 
-enum asset_type {texture, sound, animation, script};
+// in this, texture = 0, sound = 1, etc.
+// enum asset_type {texture, sound, animation, script};
+enum class asset_type {texture, sound, animation, script};
+enum class menu_section {background, music, sound, controls, texture};
 
 int main(){
-    int asset_value;
+    // this overrides the c-style enum value
+    // also the compiler doesn't realize this duality
+    int sound = 8;
+    asset_type asset_value;
 
-    asset_value = sound;
+    asset_value = asset_type::sound;
 
-    std::cout << "asset_value = " << asset_value << std::endl;
+    std::cout << "asset_value = " << (int) asset_value << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
